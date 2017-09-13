242045894099937c430578dac9497a00c888e877
kavyasai98 <31766118+kavyasai98@users.noreply.github.com>
2017-09-13 17:01:26 +0530

    Add files via upload

---
+++ organs.php
@@ -0,0 +1,38 @@
+<html>
+<center><h2 style="color:green;">DONATION OF ORGANS</h2>
+<body>
+<style>
+h2{
+	display:inline;
+}
+p{
+background.Image:url('koala');
+}
+</style>
+<table>
+<tr>
+<td>Name of the organ donor</td>
+<td><input type="text" placeholder="name"></td>
+</tr>
+<tr>
+<td>Mobile no.</td>
+<td><input type="number" placeholder="number"></td>
+</tr>
+<tr>
+<td>Organs</td>
+<td>
+<input type="checkbox" value="name">All Organs
+<input type="checkbox" value="name">pancreas
+<input type="checkbox" value="name">lungs
+<input type="checkbox" value="name">muscles
+<input type="checkbox" value="name">kidney
+<input type="checkbox" value="name">brain
+<input type="checkbox" value="name">eye
+<input type="checkbox" value="name">heart</td>
+</tr>
+<td><input type="Submit" value="submit"></td>
+<td><input type="Submit" value="cancel"></td>
+<table>
+<body>
+</center>
+<html>
